e433075c55cce735dffceb972102c70d5452df1d
MgArreaza13 <mg.arreaza.13@gmail.com>
2018-05-18 17:11:45 -0400

    ejemplos de python

---
+++ inicio.py
@@ -0,0 +1,92 @@
+#variables
+#operaciones aritmeticas
+#estructuras dde control condicionales
+#estructuras de control ciclicas
+#funciones
+
+
+nombre = 'Miguel'
+numero1 = 0
+numero2 = 17.8
+
+#arreglos = ['miguel', 24, 'M', 'Casado', 25]
+
+
+#sresultado = numero1 + numero2
+
+#print(arreglos)
+
+
+###Operaciones condicionales 
+	#operadores concionales
+	# && = y = and
+	# || = o = or
+	# == , = igual , equal
+	# != . distinto 
+	# === . exactamente igual 
+	# > = mayor
+	# < = menor 
+	# >= mayor igual
+	# <= menor igual
+
+	#identado
+	# { } 4 espacios o un tab 
+
+
+
+#usuario  = "miguel"
+#password = "miguel123"
+#rol = 'adorador'
+
+#if (usuario == 'miguel' and password == "miguel123"):
+ #	print('bienvenido a nuestra app')
+ #	if(rol == 'adorador'):
+# 		print('sistema de adorador')
+#else:
+# 	print('usuario no verificado')
+
+
+
+''
+
+arreglo = [1,2,3,4,5,6,7,8,9,10]
+
+#print(arreglo[5])
+
+#for x in range(0,len(arreglo)):
+	#print(arreglo[x])
+#	pass
+
+
+
+
+
+
+
+
+
+
+def Suma(x,y):
+	return x+y
+
+def Resta(x,y):
+	return x-y
+
+def Division(x,y):
+	if(y == 0):
+		print('error de division')
+	else:
+		return x/y
+
+print(Suma(12,100))
+print(Resta(12,100))
+print(Division(12,0))
+
+
+
+
+
+
+
+
+
